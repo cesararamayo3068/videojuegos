dc1d54ec559557f300f7da4007c1d0be2ea0ee65
Cesar Aramayo <cesar_aramayo@hotmail.es>
2022-01-29 15:47:53 -0300

    mapeando la entidad Videojuego con jpa a la base de datos

--- src/main/java/com/prog/videojuegos/domain/Videojuego.java
+++ src/main/java/com/prog/videojuegos/domain/Videojuego.java
@@ -1,13 +1,25 @@
 package com.prog.videojuegos.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-
+@Entity 
 public class Videojuego {
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 	private String nombre;
 	private String descripcion;
 	private String imagenUrl;
 	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public String getNombre() {
 		return nombre;
 	}
