194d14d698d341f24683a39238c3bfcdd474648f
Cesar Aramayo <cesar_aramayo@hotmail.es>
2022-01-29 20:27:22 -0300

    creando el formulario para buscar por nombre

--- src/main/java/com/prog/videojuegos/controller/ListadoController.java
+++ src/main/java/com/prog/videojuegos/controller/ListadoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.prog.videojuegos.domain.Videojuego;
 import com.prog.videojuegos.services.VideojuegoService;
@@ -31,5 +32,11 @@ public class ListadoController {
 		model.addAttribute("videojuegos", juegos);
 		return "listado";
 
+	}
+	@RequestMapping("/buscar")
+	public String buscar(@RequestParam("q") String consulta, Model model) {
+		List<Videojuego> juegos = videojuegoService.buscar(consulta);
+		model.addAttribute("videojuegos", juegos);
+		return "listado";
 	}
 }
